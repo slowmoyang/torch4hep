b5ff49d2e267784be878174a2aa7ce51a6dfcf98
slowmoyang <slowmoyang@gmail.com>
2019-01-30 00:11:52 +0900

    Change the name of a script

--- utils/third_libs.py
+++ utils/third_libs.py
@@ -6,6 +6,7 @@ import ROOT
 from ROOT import gInterpreter
 from ROOT import gSystem
 
+
 def load_delphes(delphes_dir):
     if hasattr(ROOT, "Delphes"):
         print("'delphes' was already loaded.")
@@ -22,3 +23,20 @@ def load_delphes(delphes_dir):
     gSystem.Load(so_file)
     gInterpreter.Declare('#include "classes/DelphesClasses.h"')
 
+
+def load_fastjet(fastjet_dir=None):
+    if hasattr(ROOT, "fastjet"):
+        print("'fastjet' was already loaded.")
+        return None
+    lib_dir = gSystem.ConcatFileName(fastjet_dir, "lib")
+    include_dir = gSystem.ConcatFileName(fastjet_dir, "include")
+
+    libfastjet_so = gSystem.ConcatFileName(lib_dir, "libfastjet.so")
+    libfastjettools_so = gSystem.ConcatFileName(lib_dir, "libfastjettools.so")
+
+    gInterpreter.AddIncludePath(include_dir)
+    gSystem.Load(libfastjet_so)
+    gSystem.Load(libfastjettools_so)
+    gInterpreter.Declare('#include "fastjet/PseudoJet.hh"')
+    gInterpreter.Declare('#include "fastjet/ClusterSequence.hh"')
+    gInterpreter.Declare('#include "fastjet/Selector.hh"')
