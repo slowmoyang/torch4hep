53e9142530b6182b8cf8edde3193dfdaeccc911c
slowmoyang <slowmoyang@gmail.com>
2019-02-04 18:51:19 +0900

    Update torch4hep.module.__init__.py

---
+++ modules/convolution/__init__.py
@@ -0,0 +1,7 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from .conv2d_same_pad import Conv2dSamePad
+from .conv2d_unit import Conv2dUnit
+from .conv2d_local import Conv2dLocal
