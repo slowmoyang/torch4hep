fccb0608a56ecf9ad885469fdadb913e3955fe60
slowmoyang <slowmoyang@gmail.com>
2019-02-04 18:51:53 +0900

    Update torch4hep.module.__init__.py

--- modules/__init__.py
+++ modules/__init__.py
@@ -2,4 +2,15 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-from .conv2dlocal import Conv2dLocal
+from torch4hep.modules.activation import Activation
+from torch4hep.modules.convolution import *
+
+from torch4hep.modules import resnet
+from torch4hep.modules import inception
+from torch4hep.modules import densenet
+from torch4hep.modules import squeeze_excitation
+from torch4hep.modules import transformer
+
+# TODO: NALU
+# TODO: NeuralProcess
+# TODO: CapsuleNet
